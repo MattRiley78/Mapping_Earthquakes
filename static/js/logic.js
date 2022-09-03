79920d036f3a789b8ee4f546c4b16a56fe406a7a
MattRiley78 <bassfacer@yahoo.com>
2022-09-02 21:55:16 -0500

    Mapping Lines

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,6 +5,21 @@ let map = L.map("mapid", {
     ],
     zoom: 5
   });
+
+// Coordinates for each point to be used in the line.
+let line =[
+  [37.6213, -122.3790],
+  [30.1975, -97.6664],
+  [35.0421, -89.9792],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
+];
+// Create a polyline using the line coordinates and make the line blue and dashed.
+L.polyline(line, {
+  color: "blue",
+  dashArray: '10, 10'
+}).addTo(map);
+
 // TILE LAYER METHOD 1:  FROM LEAFLET QUICK START GUIDE
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -19,7 +34,7 @@ let map = L.map("mapid", {
 
 
 // TILE LAYER METHOD 2: MAPBOX STYLES API
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
